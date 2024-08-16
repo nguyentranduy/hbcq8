b7c4a83e01832acdc7553877b0d44b1f681b24a4
NTDUY <nguyentranduy94@gmail.com>
2024-08-16 21:19:08 +0700

    init project
    init project

---
+++ src/main/java/com/hbc/Hoibocauq8Application.java
@@ -0,0 +1,13 @@
+package com.hbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoibocauq8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoibocauq8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbc/Hoibocauq8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hoibocauq8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
