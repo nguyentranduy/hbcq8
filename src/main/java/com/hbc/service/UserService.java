a6bf27144980caac1ee0530148ac3991dc5c1409
NTDUY <nguyentranduy94@gmail.com>
2024-08-19 22:06:39 +0700

    implt login function

--- src/main/java/com/hbc/service/UserService.java
+++ src/main/java/com/hbc/service/UserService.java
@@ -1,5 +1,8 @@
 package com.hbc.service;
 
+import com.hbc.entity.User;
+
 public interface UserService {
 
+	User doLogin(String username, String password);
 }
--- src/main/java/com/hbc/service/impl/UserServiceImpl.java
+++ src/main/java/com/hbc/service/impl/UserServiceImpl.java
@@ -1,10 +1,33 @@
 package com.hbc.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.hbc.entity.User;
+import com.hbc.repo.UserRepo;
 import com.hbc.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepo repo;
+	
+	private final BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
 
+	@Override
+	public User doLogin(String username, String password) {
+		Optional<User> userResponseOptional = repo.findByUsername(username);
+
+		if (userResponseOptional.isEmpty()) {
+			return null;
+		}
+
+		User userResponse = userResponseOptional.get();
+		
+		return bcrypt.matches(password, userResponse.getPassword()) ? userResponse : null;
+	}
 }
