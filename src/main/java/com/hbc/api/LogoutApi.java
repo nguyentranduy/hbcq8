84cad4b7ec1fdfe46d9eeaba8fd901ce48ce299a
NTDUY <nguyentranduy94@gmail.com>
2024-09-23 17:00:48 +0700

    [hotfix] refactor code

--- src/main/java/com/hbc/api/LogoutApi.java
+++ src/main/java/com/hbc/api/LogoutApi.java
@@ -2,6 +2,7 @@ package com.hbc.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/v1/logout")
+@CrossOrigin("*")
 public class LogoutApi {
 
 	@GetMapping()
